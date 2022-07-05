0c01b2719e502c1683d463b740d045f9ef4cbee8
bundisim <bundisim@students.zhaw.ch>
2022-07-05 13:44:41 +0200

    if check für total

--- frontend/src/composables/useProducts.ts
+++ frontend/src/composables/useProducts.ts
@@ -10,43 +10,45 @@ export function useProducts() {
     const newProduct = ref<Product>({});
 
     const getProducts = function () {
-        products.value = [
-            {
-                id: 1,
-                productName: 'Essen 1',
-                productDescription: 'Beschreibung 1',
-                productCalories: 500,
-                productConsumeDate: new Date().toISOString().split('T')[0],
-                productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
-                productEan: '4 003994 155486',
-            },
-            {
-                id: 2,
-                productName: 'Essen 2',
-                productDescription: 'Beschreibung 2',
-                productCalories: 150,
-                productConsumeDate: new Date().toISOString().split('T')[0],
-                productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
-                productEan: '4 003994 133675',
-            },
-            {
-                id: 3,
-                productName: 'Essen 3',
-                productDescription: 'Beschreibung 3',
-                productCalories: 300,
-                productConsumeDate: new Date().toISOString().split('T')[0],
-                productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
-                productEan: '4 003994 199862',
-            },
-            {
-                id: 4,
-                productName: 'Essen 4',
-                productDescription: 'Beschreibung 4',
-                productCalories: 200,
-                productConsumeDate: new Date().toISOString().split('T')[0],
-                productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
-                productEan: '4 003994 477166',
-            },]
+        products.value =
+            [
+                {
+                    id: 1,
+                    productName: 'Essen 1',
+                    productDescription: 'Beschreibung 1',
+                    productCalories: 500,
+                    productConsumeDate: new Date().toISOString().split('T')[0],
+                    productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
+                    productEan: '4 003994 155486',
+                },
+                {
+                    id: 2,
+                    productName: 'Essen 2',
+                    productDescription: 'Beschreibung 2',
+                    productCalories: 150,
+                    productConsumeDate: new Date().toISOString().split('T')[0],
+                    productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
+                    productEan: '4 003994 133675',
+                },
+                {
+                    id: 3,
+                    productName: 'Essen 3',
+                    productDescription: 'Beschreibung 3',
+                    productCalories: 300,
+                    productConsumeDate: new Date().toISOString().split('T')[0],
+                    productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
+                    productEan: '4 003994 199862',
+                },
+                {
+                    id: 4,
+                    productName: 'Essen 4',
+                    productDescription: 'Beschreibung 4',
+                    productCalories: 200,
+                    productConsumeDate: new Date().toISOString().split('T')[0],
+                    productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
+                    productEan: '4 003994 477166',
+                },
+            ]
     }
 
     const addNewProduct = function () {
@@ -59,6 +61,14 @@ export function useProducts() {
             productConsumeTime: new Date().toLocaleTimeString().slice(0, 5),
             productEan: '',
         };
+    }
+
+    const checkAnyProductsToday = function () {
+        if (products.value.length > 0) {
+            return true
+        } else {
+            return false
+        }
     }
 
     const showConfirmDeletionAlert = async function (product: Product) {
@@ -104,5 +114,7 @@ export function useProducts() {
         calculateDailyTotalCalories,
         deleteProduct,
         showConfirmDeletionAlert,
+        checkAnyProductsToday
+
     }
 }
